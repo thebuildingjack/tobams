e0e4da225accb429e682018f92545f725b08cdaf
Francis Authority <francis.i.authority@gmail.com>
2026-09-06 10:43:25 +0100

    ...

--- components/WhatWeDo.tsx
+++ components/WhatWeDo.tsx
@@ -4,14 +4,14 @@ import Image from "next/image"
 const WhatWeDo = () => {
   return (
     <section className="w-full relative bg-[#f9f9f9] py-16 sm:py-24 px-6">
-      <ul className="flex flex-col gap-8 max-w-7xl mx-auto">
+      <ul className="flex flex-col items-center justify-center gap-8 max-w-7xl">
         {whatwedo.map((item, index) => (
           <li key={index} className={`flex w-full flex-row gap-6 md:gap-12 items-center justify-center" ${index === 1 ? "md:flex-row-reverse" : ""}`}>
             <div className="flex flex-col gap-4 md:w-1/2">
-              <h3>{item.title}</h3>
+              <h3 className="text-start">{item.title}</h3>
               <Image src={item.img} alt={item.title} width={400} height={400} className="md:hidden w-full h-auto" />
 
-              <p>{item.desc}</p>
+              <p className="text-center">{item.desc}</p>
               <ul className=" mt-4">
                 {item.bullets.map((bullet, bulletIndex) => (
                   <li key={bulletIndex} className="flex flex-row gap-2 items-center">
